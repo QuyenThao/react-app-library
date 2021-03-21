c33c0fdf52866876730a4953c4ef15a5f379a0da
Quyenthao <thaoquyen23012003@gmail.com>
2021-03-21 07:57:30 +0700

    library

---
+++ my-app/build/precache-manifest.d1f3015f20d29ccf0f723fb7d8578198.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3abcc45fb17f769fbc53fbf037e847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb957a290e6586d98d64",
+    "url": "/static/css/main.68bf912b.chunk.css"
+  },
+  {
+    "revision": "9599690a6eda61f47a91",
+    "url": "/static/js/2.be949c01.chunk.js"
+  },
+  {
+    "revision": "f0d0855484a7a377ebb232589c642562",
+    "url": "/static/js/2.be949c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb957a290e6586d98d64",
+    "url": "/static/js/main.35c89da2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "3de8014e8bbd35af51ca61f2339804e6",
+    "url": "/static/media/aut1.3de8014e.jpg"
+  },
+  {
+    "revision": "7a46e4ac448cd40638aa4abe7f1e6bf2",
+    "url": "/static/media/aut2.7a46e4ac.jpg"
+  },
+  {
+    "revision": "097108bc410d045ecd0b72ed55cd279a",
+    "url": "/static/media/aut3.097108bc.png"
+  },
+  {
+    "revision": "67d372843b5ef7556aad1563240ccdc1",
+    "url": "/static/media/aut6.67d37284.png"
+  },
+  {
+    "revision": "3915629d0033706160d974dc2de4bbf5",
+    "url": "/static/media/aut7.3915629d.jpg"
+  },
+  {
+    "revision": "2d2e12bcdc4027fa732b1f80615fae17",
+    "url": "/static/media/aut8.2d2e12bc.jpg"
+  },
+  {
+    "revision": "d502a456608ca2dcfb29e9ce5e71925f",
+    "url": "/static/media/aut9.d502a456.png"
+  },
+  {
+    "revision": "130d2fe461ff689c82886fb5d8e8a8fb",
+    "url": "/static/media/avatar-1.130d2fe4.png"
+  },
+  {
+    "revision": "66099c3134c2d7d481d1c881ec1fb493",
+    "url": "/static/media/avatar-2.66099c31.png"
+  },
+  {
+    "revision": "35cc4017668e27db7c0413c3f2b80206",
+    "url": "/static/media/avatar-3.35cc4017.png"
+  },
+  {
+    "revision": "4cc03ca0a57713b46067175e89e4ecf9",
+    "url": "/static/media/avatar-4.4cc03ca0.png"
+  },
+  {
+    "revision": "df167faf463974351976be5d45707c93",
+    "url": "/static/media/error404.df167faf.jpg"
+  },
+  {
+    "revision": "2fb50f15d454f59ecc12a60ef599b7dc",
+    "url": "/static/media/gallery-1.2fb50f15.jpg"
+  },
+  {
+    "revision": "7bdff3ca5f37d01a31c7ba853b9a3000",
+    "url": "/static/media/gallery-10.7bdff3ca.jpg"
+  },
+  {
+    "revision": "38b707c7e1efd0d40d267cdf336612c9",
+    "url": "/static/media/gallery-11.38b707c7.jpg"
+  },
+  {
+    "revision": "00e94c3103349443c12a034594b517b6",
+    "url": "/static/media/gallery-12.00e94c31.jpg"
+  },
+  {
+    "revision": "8198e85725cb800f6740c7dcf5261fee",
+    "url": "/static/media/gallery-2.8198e857.jpg"
+  },
+  {
+    "revision": "aa9954cc905dda070c03817ba327fe47",
+    "url": "/static/media/gallery-3.aa9954cc.jpg"
+  },
+  {
+    "revision": "a7205a01a55f0627e4a11ea435650265",
+    "url": "/static/media/gallery-4.a7205a01.jpg"
+  },
+  {
+    "revision": "75e000673c7c5c60ed05589347350aa5",
+    "url": "/static/media/gallery-5.75e00067.jpg"
+  },
+  {
+    "revision": "28103257dd59caaa50a9857b68ed19d5",
+    "url": "/static/media/gallery-6.28103257.jpg"
+  },
+  {
+    "revision": "2e596342f9d0ced356b90333fa4281ff",
+    "url": "/static/media/gallery-7.2e596342.jpg"
+  },
+  {
+    "revision": "5d65c58fa47d0424136cf522f3b70453",
+    "url": "/static/media/gallery-8.5d65c58f.jpg"
+  },
+  {
+    "revision": "0843209995da3b46a70a6ac483783ffa",
+    "url": "/static/media/gallery-9.08432099.jpg"
+  },
+  {
+    "revision": "60aaf4eb8ac456288294d6a046b67fef",
+    "url": "/static/media/home.60aaf4eb.jpg"
+  },
+  {
+    "revision": "e7e24428aec5747fbb7e45c7699a167d",
+    "url": "/static/media/loading-spinner.e7e24428.gif"
+  },
+  {
+    "revision": "8a294183dc3449021606b4e4816ff057",
+    "url": "/static/media/product1.8a294183.jpg"
+  },
+  {
+    "revision": "8e02e8003d1b6116aac81f57faa52bc2",
+    "url": "/static/media/product10.8e02e800.jpg"
+  },
+  {
+    "revision": "979564961f5b74863b2df7beec39a6c5",
+    "url": "/static/media/product11.97956496.jpg"
+  },
+  {
+    "revision": "3b7acb3e6800035b03044dd24f689516",
+    "url": "/static/media/product12.3b7acb3e.jpg"
+  },
+  {
+    "revision": "a8618cb1215dcc88dd15d03960980d53",
+    "url": "/static/media/product13.a8618cb1.jpg"
+  },
+  {
+    "revision": "3dbd8b38e3cd62fdfd3a344b5a19a4d1",
+    "url": "/static/media/product2.3dbd8b38.jpg"
+  },
+  {
+    "revision": "c24c157a3862f63c59c1d497da70ece6",
+    "url": "/static/media/product3.c24c157a.jpg"
+  },
+  {
+    "revision": "b69b10a41feb614aa1de7d63e1daf8c6",
+    "url": "/static/media/product4.b69b10a4.jpg"
+  },
+  {
+    "revision": "61ddadadd2bed5e6438363d7b39473b6",
+    "url": "/static/media/product5.61ddadad.jpg"
+  },
+  {
+    "revision": "85cfdeb18677a186aa1143e324236305",
+    "url": "/static/media/product6.85cfdeb1.jpg"
+  },
+  {
+    "revision": "75b90d60b82c8216ed31e205a33b6d37",
+    "url": "/static/media/product7.75b90d60.jpg"
+  },
+  {
+    "revision": "b96e853ad5d02b3d4c2414aad9cfa5bc",
+    "url": "/static/media/product8.b96e853a.jpg"
+  },
+  {
+    "revision": "cc9d6758372f6e390062dbe4a4b730c3",
+    "url": "/static/media/product9.cc9d6758.jpg"
+  },
+  {
+    "revision": "ac027b2b7c81d81985fc83d1f50ff3c1",
+    "url": "/static/media/services-bg-img.ac027b2b.jpg"
+  },
+  {
+    "revision": "9c64589addecfc1900ac0d6c25a0e8b2",
+    "url": "/static/media/shoplist.9c64589a.jpg"
+  },
+  {
+    "revision": "8444044cc0546e6cd8d2ad8c0faf4f78",
+    "url": "/static/media/sign1.8444044c.svg"
+  },
+  {
+    "revision": "b77d82d6505f3c2f3b4bb779299745b6",
+    "url": "/static/media/sign2.b77d82d6.svg"
+  },
+  {
+    "revision": "158e063e961e252e8a4e74dff59ba746",
+    "url": "/static/media/sign3.158e063e.svg"
+  },
+  {
+    "revision": "e8bd54077bb4cc8fdc8336abbb6ee64e",
+    "url": "/static/media/sign4.e8bd5407.svg"
+  },
+  {
+    "revision": "ac653d60a115b2e6bf98fa25f102229f",
+    "url": "/static/media/team-1.ac653d60.png"
+  },
+  {
+    "revision": "cb76faec94b4dacfc0311ad0bc7bf6f5",
+    "url": "/static/media/team-2.cb76faec.png"
+  },
+  {
+    "revision": "8d84274294cd890b09136f9d56743412",
+    "url": "/static/media/team-3.8d842742.png"
+  },
+  {
+    "revision": "b668019f60982ebb5a823f85a771b6f7",
+    "url": "/static/media/team-4.b668019f.png"
+  }
+]);
---
+++ my-app/src/components/SearchProduct.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import product1 from "../images/product1.jpg";
+
+export default function SearchProduct({ product }) {
+  const { name, image } = product;
+  return (
+    <div className="search-product">
+      <div className="search-product-img">
+        <img src={image} alt="" />
+      </div>
+      <div className="search-product-details">
+        <div className="search-product-title">
+          <Link to="/">{name}</Link>
+        </div>
+        <div className="search-product-text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi…
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
